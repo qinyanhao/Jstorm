2c619c1d2e690c67ad60e00cb69d346995a72146
qinyanhao <sylvia.wang4837@gmail.com>
2021-09-13 22:31:20 +0800

    完成回應句

--- Jstorm_infoBot2.py
+++ Jstorm_infoBot2.py
@@ -33,6 +33,11 @@ arashi=['相葉雅紀','松本潤','二宮和也','大野智','櫻井翔']
 kattun=['亀梨和也','上田竜也','中丸雄一']
 jump=['山田涼介','知念侑李','中島裕翔','有岡大貴','髙木雄也','伊野尾慧','八乙女光','薮宏太']
 
+ageDICT={"TOKIO":['城島茂','国分太一','松岡昌宏'],
+         "嵐":['大野智','櫻井翔','相葉雅紀','二宮和也','松本潤'],
+         "KAT-TUN":['中丸雄一','上田竜也','亀梨和也'],
+         "Hey! Say! JUMP":['薮宏太','髙木雄也','伊野尾慧','八乙女光','有岡大貴','山田涼介','中島裕翔','知念侑李']}
+
 punctuationPat = re.compile("[,\.\?:;，。？、：；\n]+")
 def getLokiResult(inputSTR):
     punctuationPat = re.compile("[,\.\?:;，。？、：；\n]+")
@@ -69,6 +74,29 @@ def findAge(name): #計算歲數
     age= math.floor((today-birthday).days/365)
     
     return age
+
+def findHeight(name):
+    '''
+    輸入：全名
+    輸出：身高(int)
+    '''
+    if name in tokio:
+        for n in range(len(ProfileDICT["TOKIO"])):
+           if name == ProfileDICT["TOKIO"][n]['JName']:
+               height=ProfileDICT["TOKIO"][n]['height']
+    if name in arashi:
+         for n in range(len(ProfileDICT["嵐"])):
+            if name == ProfileDICT["嵐"][n]['JName']:
+                height=ProfileDICT["嵐"][n]['height']                             
+    if name in kattun:
+         for n in range(len(ProfileDICT["KAT-TUN"])):
+            if name == ProfileDICT["KAT-TUN"][n]['JName']:
+                height=ProfileDICT["KAT-TUN"][n]['height']   
+    if name in jump:
+         for n in range(len(ProfileDICT["Hey! Say! JUMP"])):
+            if name == ProfileDICT["Hey! Say! JUMP"][n]['JName']:
+                height=ProfileDICT["Hey! Say! JUMP"][n]['height']  
+    return height
 
 def maxIndex(list): #找最大值的index
     index=[]
@@ -162,22 +190,47 @@ async def on_message(message):
 
                 replySTR = "有以下團體：\n"+answerSTR
                 mscDICT[client.user.id]["completed"]=True
-        if type(mscDICT[client.user.id]["group"])==str and mscDICT[client.user.id]["request"] in ('yes','no'):
+        elif type(mscDICT[client.user.id]["group"])==str and mscDICT[client.user.id]["request"] in ('yes','no'):
             if mscDICT[client.user.id]["request"]=='yes':
                 replySTR = '沒錯，正是如此。'
             else: 
                 replySTR = mscDICT[client.user.id]["group"]+' 並不是Jstorm旗下的團體。'
         
         #member
-        if type(mscDICT[client.user.id]["member"])==list:
-            answerSTR=""
-            for n in range(len(mscDICT[client.user.id]["member"])):
-                answerSTR+=lokiResultDICT['member'][n]+lokiResultDICT['request'][n]+'\n'
-    
-            replySTR = "有以下成員：\n"+answerSTR
-            mscDICT[client.user.id]["completed"]=True
+        elif type(mscDICT[client.user.id]["member"])==list:
+            if type(mscDICT[client.user.id]["request"])==list:
+                for e in mscDICT[client.user.id]["request"]:
+                    if e in ("A型","B型","O型","AB型"):  #問各血型
+                        answerSTR=""
+                        for n in range(len(mscDICT[client.user.id]["member"])):
+                            answerSTR+=lokiResultDICT['member'][n]+' 是 '+lokiResultDICT['request'][n]+'。\n'
+                        replySTR = answerSTR
+                        mscDICT[client.user.id]["completed"]=True
+                        
+                    else:  #問整團成員
+                        answerSTR=""
+                        for n in range(len(mscDICT[client.user.id]["member"])):
+                            answerSTR+=lokiResultDICT['member'][n]+lokiResultDICT['request'][n]+'\n'
+                        replySTR = "有以下成員：\n"+answerSTR
+                        mscDICT[client.user.id]["completed"]=True
+                
+                
+            else: #type(mscDICT[client.user.id]["request"])!=list   
+                if len(mscDICT[client.user.id]["request"])>2:#問特定出生地
+                    answerSTR=""
+                    for n in range(len(mscDICT[client.user.id]["member"])):
+                        answerSTR+=lokiResultDICT['member'][n]+" "
+                    replySTR =answerSTR+"來自 "+mscDICT[client.user.id]["request"]+"。"
+                    mscDICT[client.user.id]["completed"]=True
+                else: #問特定血型
+                    answerSTR=""
+                    for n in range(len(mscDICT[client.user.id]["member"])):
+                        answerSTR+=lokiResultDICT['member'][n]+" "
+                    replySTR =answerSTR+"是 "+mscDICT[client.user.id]["request"]+"。"
+                    mscDICT[client.user.id]["completed"]=True
+                
         
-        if type(mscDICT[client.user.id]["member"])==str:
+        elif type(mscDICT[client.user.id]["member"])==str:
             if mscDICT[client.user.id]["request"] == 'yes': #成員和團體有對上
                 replySTR='沒錯，正是如此。'
                 mscDICT[client.user.id]["completed"]=True      
@@ -216,7 +269,7 @@ async def on_message(message):
                     replySTR=ProfileDICT[mscDICT[client.user.id]["group"]][index]['ELname']
                     mscDICT[client.user.id]["completed"]=True
             
-                #age
+            #age
             elif mscDICT[client.user.id]["request"] in ("age","age.max","age.min","age.sort.HtoL","age.sort.LtoH"):
                 ageLIST=[]
                 for n in range(len(ProfileDICT[mscDICT[client.user.id]["group"]])) :
@@ -226,7 +279,7 @@ async def on_message(message):
                     replySTR=str(findAge(mscDICT[client.user.id]["member"]))+"歲"
                     mscDICT[client.user.id]["completed"]=True
                     
-                if mscDICT[client.user.id]["request"] == 'age.max': #回報最年長
+                elif mscDICT[client.user.id]["request"] == 'age.max': #回報最年長
                     indexLIST=maxIndex(ageLIST)
                     if len(indexLIST) != 1:  #如果很多人同歲比較生日
                         birthLIST=[]
@@ -241,7 +294,7 @@ async def on_message(message):
                         mscDICT[client.user.id]["completed"]=True
                         
                         
-                if mscDICT[client.user.id]["request"] == 'age.min': #回報最年幼
+                elif mscDICT[client.user.id]["request"] == 'age.min': #回報最年幼
                     indexLIST=minIndex(ageLIST)
                     if len(indexLIST) != 1: #如果很多人同歲比較生日
                         birthLIST=[]
@@ -255,12 +308,192 @@ async def on_message(message):
                         replySTR="是 "+ProfileDICT[mscDICT[client.user.id]["group"]][index]['JName']+f" ，他 {ageLIST[index]} 歲。"
                         mscDICT[client.user.id]["completed"]=True
                         
-                if mscDICT[client.user.id]["request"] == 'age.sort.HtoL':
+                elif mscDICT[client.user.id]["request"] == 'age.sort.HtoL': #年齡從大排到小
+                    groupLIST=ageDICT[mscDICT[client.user.id]["group"]]
+                    answerSTR=""
+                    for n in range(len(groupLIST)):
+                        answerSTR+=groupLIST[n]+' ， '+str(findAge(groupLIST[n]))+' 歲。\n'
+                    replySTR='年齡從大排到小為：\n'+answerSTR
+                    mscDICT[client.user.id]["completed"]=True
+
+                elif mscDICT[client.user.id]["request"] == 'age.sort.LtoH':  #年齡從小排到大
+                    ageDICT[mscDICT[client.user.id]["group"]].reverse()
+                    groupLIST=ageDICT[mscDICT[client.user.id]["group"]]
+                    answerSTR=""
+                    for n in range(len(groupLIST)):
+                        answerSTR+=groupLIST[n]+' ， '+str(findAge(groupLIST[n]))+' 歲。\n'
+                    replySTR='年齡從小排到大為：\n'+answerSTR
+                    mscDICT[client.user.id]["completed"]=True
                     
-        
-        
-        
-        
+                    
+            elif mscDICT[client.user.id]["request"] in ("year","month","day"): #問年月日
+                if mscDICT[client.user.id]["member"] in tokio:
+                    for n in range(len(tokio)):
+                        if mscDICT[client.user.id]["member"]==ProfileDICT["TOKIO"][n]["JName"]:
+                            birthday = ProfileDICT["TOKIO"][n]["Birth"].split(".")
+                elif mscDICT[client.user.id]["member"] in arashi:
+                    for n in range(len(tokio)):
+                        if mscDICT[client.user.id]["member"]==ProfileDICT["嵐"][n]["JName"]:
+                            birthday = ProfileDICT["嵐"][n]["Birth"].split(".")
+                elif mscDICT[client.user.id]["member"] in kattun:
+                    for n in range(len(tokio)):
+                        if mscDICT[client.user.id]["member"]==ProfileDICT["KAT-TUN"][n]["JName"]:
+                            birthday = ProfileDICT["KAT-TUN"][n]["Birth"].split(".")
+                elif mscDICT[client.user.id]["member"] in jump:
+                    for n in range(len(tokio)):
+                        if mscDICT[client.user.id]["member"]==ProfileDICT["Hey! Say! JUMP"][n]["JName"]:
+                            birthday = ProfileDICT["Hey! Say! JUMP"][n]["Birth"].split(".")
+            
+                if mscDICT[client.user.id]["request"] == 'year':
+                    replySTR='他是 '+birthday[0]+'年出生的。'
+                    mscDICT[client.user.id]["completed"]=True
+                elif mscDICT[client.user.id]["request"] == 'month':
+                    replySTR='他是 '+birthday[1]+'月出生的。'
+                    mscDICT[client.user.id]["completed"]=True
+                elif mscDICT[client.user.id]["request"] == 'day':
+                    replySTR='他是 '+birthday[2]+'日出生的。'
+                    mscDICT[client.user.id]["completed"]=True
+                    
+                   
+            elif mscDICT[client.user.id]["request"] in ("height.max","height.min","height.sort.HtoL","height.sort.LtoH"):
+                heightLIST=[]
+                for n in range(len(ProfileDICT[mscDICT[client.user.id]["group"]])):
+                    heightLIST.append(ProfileDICT[mscDICT[client.user.id]["group"]][n]['height'])
+                    
+                if mscDICT[client.user.id]["request"] =="height": #身高
+                    replySTR= mscDICT[client.user.id]["member"]+"，他 "+str(findHeight(mscDICT[client.user.id]["member"]))+" cm。"
+                    mscDICT[client.user.id]["completed"]=True
+                
+                
+                if mscDICT[client.user.id]["request"] =="height.max":  #最高
+                    indexLIST=maxIndex(heightLIST)
+                    answerSTR=""
+                    for n in indexLIST:
+                        answerSTR += ProfileDICT[mscDICT[client.user.id]["group"]][n]["JName"]+"，他 "+ str(heightLIST[n])+ "cm。\n"
+                    replySTR = answerSTR
+                    mscDICT[client.user.id]["completed"]=True
+                    
+                elif mscDICT[client.user.id]["request"] =="height.min":  #最矮
+                    indexLIST=minIndex(heightLIST)
+                    answerSTR=""
+                    for n in indexLIST:
+                        answerSTR+=ProfileDICT[mscDICT[client.user.id]["group"]][n]["JName"]+"，他 "+str(heightLIST[n])+ "cm。\n"
+                    replySTR =answerSTR
+                    mscDICT[client.user.id]["completed"]=True
+
+                elif mscDICT[client.user.id]["request"] =="height.sort.LtoH":  #矮到高
+                    sortedLIST=sorted(heightLIST)
+                    indexLIST=[]
+                    for n in range(len(heightLIST)):
+                        indexLIST.append(heightLIST.index(sortedLIST[n]))
+                    lostIndex=[]
+                    for n in range(min(indexLIST),max(indexLIST)):
+                        if n not in indexLIST:
+                            lostIndex.append(n)
+                    for e in indexLIST:
+                        if indexLIST.count(e)>1:
+                            for l in lostIndex:
+                             indexLIST[indexLIST.index(e)]= l
+                    answerSTR=""
+                    for n in indexLIST:
+                        answerSTR += ProfileDICT[mscDICT[client.user.id]["group"]][n]['JName']+"，"+str(ProfileDICT[mscDICT[client.user.id]["group"]][n]['height'])+"cm\n"
+                    replySTR = "身高從矮到高是：\n"+answerSTR
+                    mscDICT[client.user.id]["completed"]=True
+                    
+                elif mscDICT[client.user.id]["request"] =="height.sort.HtoL":  #高到矮
+                    sortedLIST=sorted(heightLIST)
+                    sortedLIST.reverse()
+                    indexLIST=[]
+                    for n in range(len(heightLIST)):
+                        indexLIST.append(heightLIST.index(sortedLIST[n]))
+                    lostIndex=[]
+                    for n in range(min(indexLIST),max(indexLIST)):
+                        if n not in indexLIST:
+                            lostIndex.append(n)
+                    for e in indexLIST:
+                        if indexLIST.count(e)>1:
+                            for l in lostIndex:
+                             indexLIST[indexLIST.index(e)]= l
+                    answerSTR=""
+                    for n in indexLIST:
+                        answerSTR += ProfileDICT[mscDICT[client.user.id]["group"]][n]['JName']+"，"+str(ProfileDICT[mscDICT[client.user.id]["group"]][n]['height'])+"cm\n"
+                    replySTR = "身高從高到矮是：\n"+answerSTR
+                    mscDICT[client.user.id]["completed"]=True
+                   
+            elif mscDICT[client.user.id]["request"].encode('UTF-8').isalnum() == True: #request是身高
+                replySTR= mscDICT[client.user.id]["member"]+"，他 "+str(findHeight(mscDICT[client.user.id]["member"]))+" cm。"
+                mscDICT[client.user.id]["completed"]=True
+            
+            elif mscDICT[client.user.id]["request"].encode('UTF-8').isalpha() == False: 
+
+                if len(mscDICT[client.user.id]["request"]) == 2 :#request 為血型
+                    if mscDICT[client.user.id]["member"] == 'no': #沒有是該血型的人
+                        replySTR=mscDICT[client.user.id]["group"]+" 中沒有寫血型是 "+mscDICT[client.user.id]["request"]+" 的成員。"
+                        mscDICT[client.user.id]["completed"]=True
+                    elif mscDICT[client.user.id]["request"].isdigit() == True: #距離生日的天數是兩位數
+                        replySTR="離 "+mscDICT[client.user.id]["member"]+" 的生日還有 "+mscDICT[client.user.id]["request"]+" 天。"
+                        mscDICT[client.user.id]["completed"]=True
+                    else: #回報血型
+                        replySTR="他是 "+mscDICT[client.user.id]["request"]+" 。"
+                        mscDICT[client.user.id]["completed"]=True
+                
+                elif 2< len(mscDICT[client.user.id]["request"]) <5 : #request是地名
+                    if mscDICT[client.user.id]["member"] == 'no': #沒有來自該地的人
+                        replySTR=mscDICT[client.user.id]["group"]+" 中沒有來自 "+mscDICT[client.user.id]["request"]+" 的成員。"
+                        mscDICT[client.user.id]["completed"]=True
+                    elif mscDICT[client.user.id]["request"].isdigit() == True: #距離生日的天數是三位數
+                        replySTR="離 "+mscDICT[client.user.id]["member"]+" 的生日還有 "+mscDICT[client.user.id]["request"]+" 天。"
+                        mscDICT[client.user.id]["completed"]=True
+                    else: #回報地名
+                        replySTR="他來自 "+mscDICT[client.user.id]["request"]+" 。"
+                        mscDICT[client.user.id]["completed"]=True
+                        
+                else: #request是生日
+                    if mscDICT[client.user.id]["request"].isdigit(): #距離生日的天數是個位數
+                        replySTR="離 "+mscDICT[client.user.id]["member"]+" 的生日還有 "+mscDICT[client.user.id]["request"]+" 天。"
+                        mscDICT[client.user.id]["completed"]=True
+                    else: #request是生日
+                        replySTR="他的生日是 "+mscDICT[client.user.id]["request"]+" 。"
+                        mscDICT[client.user.id]["completed"]=True
+                
+        elif type(mscDICT[client.user.id]["request"]) == list:  #回報多個問題
+            for e in mscDICT[client.user.id]["request"]:
+                if e in ("age.max","age.min"):
+                    ageLIST=[]
+                    for n in range(len(ProfileDICT[mscDICT[client.user.id]["group"]])) :
+                        ageLIST.append(findAge(ProfileDICT[mscDICT[client.user.id]["group"]][n]['JName']))
+                    if e == 'age.max': #回報最年長
+                        maxLIST=[]
+                        indexLIST=maxIndex(ageLIST)
+                        if len(indexLIST) != 1:  #如果很多人同歲比較生日
+                            birthLIST=[]
+                            for i in indexLIST:
+                                birthLIST.append(ProfileDICT[mscDICT[client.user.id]["group"]][i]['Birth'])
+                            index=birthLIST.index(min(birthLIST))
+                            maxLIST.append(ProfileDICT[mscDICT[client.user.id]["group"]][indexLIST[index]]['JName'])
+                            maxLIST.append(ageLIST[indexLIST[index]])
+                        else: #len(indexLIST) == 1
+                            index=ageLIST.index(max(ageLIST))
+                            maxLIST.append(ProfileDICT[mscDICT[client.user.id]["group"]][index]['JName'])
+                            maxLIST.append(str(ageLIST[index]))
+                    if e == 'age.max': #回報最年長
+                        minLIST=[]
+                        indexLIST=minIndex(ageLIST)
+                        if len(indexLIST) != 1:  #如果很多人同歲比較生日
+                            birthLIST=[]
+                            for i in indexLIST:
+                                birthLIST.append(ProfileDICT[mscDICT[client.user.id]["group"]][i]['Birth'])
+                            index=birthLIST.index(max(birthLIST))
+                            minLIST.append(ProfileDICT[mscDICT[client.user.id]["group"]][indexLIST[index]]['JName'])
+                            minLIST.append(ageLIST[indexLIST[index]])
+                        else: #len(indexLIST) == 1
+                            index=ageLIST.index(min(ageLIST))
+                            minLIST.append(ProfileDICT[mscDICT[client.user.id]["group"]][index]['JName'])
+                            minLIST.append(str(ageLIST[index]))
+                    print(maxLIST)
+                    print(minLIST)
+                    replySTR=f'最年長是 {maxLIST[0]}，{maxLIST[1]} 歲。\n最年幼是 {minLIST[0]}，{minLIST[1]} 歲。'
+                    mscDICT[client.user.id]["completed"]=True
        
                 
 
