d47c23fa8707edc4d9ef5ffe6e909eb9f8c08062
qinyanhao <sylvia.wang4837@gmail.com>
2021-09-16 16:33:33 +0800

    完成多輪對話回應句

--- Jstorm_infoBot2.py
+++ Jstorm_infoBot2.py
@@ -25,8 +25,10 @@ mscDICT = {# "userID": {requestTemplate}
 with open("account.info", encoding="utf-8") as f:
     accountDICT = json.loads(f.read())
 # 另一個寫法是：accountDICT = json.load(open("account.info", encoding="utf-8"))
-with open('D:\HAO\Hao的研所\實習\Jstorm\ProfileDICT.json', 'r') as f:
+with open('D:\HAO\Hao的研所\實習\Jstorm2\ProfileDICT.json', 'r') as f:
     ProfileDICT=json.load(f)
+with open('D:\HAO\Hao的研所\實習\Jstorm2\GroupDICT.json', 'r') as f:
+    GroupDICT=json.load(f)
 
 tokio=['国分太一','城島茂','松岡昌宏']
 arashi=['相葉雅紀','松本潤','二宮和也','大野智','櫻井翔']
@@ -37,6 +39,7 @@ ageDICT={"TOKIO":['城島茂','国分太一','松岡昌宏'],
          "嵐":['大野智','櫻井翔','相葉雅紀','二宮和也','松本潤'],
          "KAT-TUN":['中丸雄一','上田竜也','亀梨和也'],
          "Hey! Say! JUMP":['薮宏太','髙木雄也','伊野尾慧','八乙女光','有岡大貴','山田涼介','中島裕翔','知念侑李']}
+
 
 punctuationPat = re.compile("[,\.\?:;，。？、：；\n]+")
 def getLokiResult(inputSTR):
@@ -122,6 +125,146 @@ def findBirthday(name):
                 birthday = ProfileDICT["Hey! Say! JUMP"][n]["Birth"]
     return birthday
 
+def findColor(name):
+    '''
+    輸入:全名
+    輸出：顏色(str)
+    '''
+    if name in tokio:
+        index=tokio.index(name)
+        color = ProfileDICT['TOKIO'][index]['color']
+        
+    if name in arashi:
+        index=arashi.index(name)
+        color = ProfileDICT['嵐'][index]['color']
+        
+    if name in kattun:
+        index=kattun.index(name)
+        color = ProfileDICT['KAT-TUN'][index]['color']
+        
+    if name in jump:
+        index=jump.index(name)
+        color = ProfileDICT['Hey! Say! JUMP'][index]['color']
+
+    return color
+
+def findColorLIST(group):
+    '''
+    輸入：團名
+    輸出：顏色(list)
+    '''
+    colorLIST=[]
+    if group=='TOKIO':
+        for n in range(len(tokio)):
+            colorLIST.append(ProfileDICT['TOKIO'][n]['color'])                 
+    if group=='嵐':
+        for n in range(len(arashi)):
+            colorLIST.append(ProfileDICT['嵐'][n]['color'])                            
+    if group=='KAT-TUN':
+        for n in range(len(kattun)):
+            colorLIST.append(ProfileDICT['KAT-TUN'][n]['color'])                              
+    if group=='Hey! Say! JUMP':
+        for n in range(len(jump)):
+            colorLIST.append(ProfileDICT['Hey! Say! JUMP'][n]['color'])
+    return colorLIST
+
+def findDate(group,anniversary):
+    '''
+    輸入:團名,'formationdate'/'debutdate'
+    輸出：日期(str)
+    '''
+    if anniversary=='formationdate':
+        if group=='TOKIO':
+            date=GroupDICT['TOKIO']['formationdate']
+        if group=='嵐':
+            date=GroupDICT['嵐']['formationdate']                 
+        if group=='KAT-TUN':
+            date=GroupDICT['KAT-TUN']['formationdate']                 
+        if group=='Hey! Say! JUMP':
+            date=GroupDICT['Hey! Say! JUMP']['formationdate']
+    elif anniversary=='debutdate':
+        if group=='TOKIO':
+            date=GroupDICT['TOKIO']['debutdate']
+        if group=='嵐':
+            date=GroupDICT['嵐']['debutdate']                     
+        if group=='KAT-TUN':
+            date=GroupDICT['KAT-TUN']['debutdate']                
+        if group=='Hey! Say! JUMP':
+            date=GroupDICT['Hey! Say! JUMP']['debutdate']            
+    return date
+
+def countDays(group,anniversary):
+    '''
+    輸入:團名,'formationdate'/'debutdate'
+    輸出:距離下一次節日的天數(int)
+    '''
+    if anniversary == 'formationdate':
+        if group == 'TOKIO':
+            date=GroupDICT[group]['formationdate'].split('.')
+            date.append('1')
+        elif group == '嵐':
+            date=GroupDICT[group]['formationdate'].split('.')
+        elif group == 'KAT-TUN':
+            date=GroupDICT[group]['formationdate'].split('.')
+        elif group == 'Hey! Say! JUMP':
+            date=GroupDICT[group]['formationdate'].split('.')
+
+    elif anniversary == 'debutdate':
+        if group == 'TOKIO':
+            date=GroupDICT[group]['debutdate'].split('.')
+        elif group == '嵐':
+            date=GroupDICT[group]['debutdate'].split('.')
+        elif group == 'KAT-TUN':
+            date=GroupDICT[group]['debutdate'].split('.')
+        elif group == 'Hey! Say! JUMP':
+            date=GroupDICT[group]['debutdate'].split('.')
+    
+    today=datetime.date.today()
+    current_anniversary= datetime.date(year=today.year, month=int(date[1]),day=int(date[2]))
+ 
+    if current_anniversary<today:
+        if today.year/4 != 0:
+            days=(current_anniversary-today).days+365
+        else:
+            days=(current_anniversary-today).days+366
+    else: #current_birthday>today
+        days=(current_anniversary-today).days
+
+    return days
+
+def countYears(group,anniversary):
+    '''
+    輸入:團名,'formationdate'/'debutdate'
+    輸出:年(int)
+    '''
+    if anniversary == 'formationdate':
+        if group == 'TOKIO':
+            date=GroupDICT[group]['formationdate'].split('.')
+        elif group == '嵐':
+            date=GroupDICT[group]['formationdate'].split('.')
+        elif group == 'KAT-TUN':
+            date=GroupDICT[group]['formationdate'].split('.')
+        elif group == 'Hey! Say! JUMP':
+            date=GroupDICT[group]['formationdate'].split('.')
+
+    elif anniversary == 'debutdate':
+        if group == 'TOKIO':
+            date=GroupDICT[group]['debutdate'].split('.')
+        elif group == '嵐':
+            date=GroupDICT[group]['debutdate'].split('.')
+        elif group == 'KAT-TUN':
+            date=GroupDICT[group]['debutdate'].split('.')
+        elif group == 'Hey! Say! JUMP':
+            date=GroupDICT[group]['debutdate'].split('.')
+    
+
+    this_year=datetime.date.today().year
+    anniversary_year=int(date[0])
+ 
+    years=this_year-anniversary_year
+
+    return years
+
 def maxIndex(list): #找最大值的index
     index=[]
     maxOne=max(list)
@@ -174,10 +317,6 @@ async def on_message(message):
                                        "completed": False,
                                        "updatetime": datetime.datetime.now()
                                               }
-            
-        if len(lokiResultDICT.keys())==1:
-            mscDICT[client.user.id]["member"] = ""
-            mscDICT[client.user.id]["request"] = ""
         #多輪對話
         if lokiResultDICT:
             for k in lokiResultDICT.keys():    # 將 Loki Intent 的結果，存進 Global mscDICT 變數，可替換成 Database。
@@ -187,10 +326,7 @@ async def on_message(message):
                     mscDICT[client.user.id]["member"] = lokiResultDICT["member"]
                 elif k == 'request':
                     mscDICT[client.user.id]["request"] = lokiResultDICT["request"]
-        
-        if len(lokiResultDICT.keys()) == 1 and lokiResultDICT.keys() == 'Group':
-            mscDICT[client.user.id]['member']=""
-            mscDICT[client.user.id]["request"]=""
+    
 
         print("mscDICT =")
         pprint(mscDICT)
@@ -198,22 +334,23 @@ async def on_message(message):
         if mscDICT[client.user.id]["request"] == "":  # 多輪對話的問句。
             replySTR = '請問你想問的是哪方面呢？（各團成員、日英姓名、生日、年齡、血型、出身地）'
         
-        elif mscDICT[client.user.id]["group"] == "" and mscDICT[client.user.id]["member"] == "": 
-            if mscDICT[client.user.id]["request"] in ("age","height","birth",'year','month','day'):
+        elif mscDICT[client.user.id]['group'] == "" and mscDICT[client.user.id]["member"] == "": 
+            if mscDICT[client.user.id]["request"] in ("age","height","birth",'year','month','day','color'):
                 replySTR = "請問您是問哪一個團體的哪位成員呢？"   
-            elif mscDICT[client.user.id]["request"] in ("age.max","age.min","age.sort.HtoL","age.sort.LtoH","height.max","height.min","height.sort.HtoL","height.sort.LtoH"):
+            elif mscDICT[client.user.id]["request"] in ("formationdate","debutdate","formationdate.years","debutdate.years","formationdate.days","debutdate.days","age.max","age.min","age.sort.HtoL","age.sort.LtoH","height.max","height.min","height.sort.HtoL","height.sort.LtoH"):
                 replySTR = "請問您是問哪一個團體的呢？"
         
-        elif mscDICT[client.user.id]["group"] == "":
+        elif mscDICT[client.user.id]["group"]=="":
             replySTR = "請問您是問哪一個團體呢？"
+        
+        elif mscDICT[client.user.id]["member"] == "" and mscDICT[client.user.id]["request"] in ("age","height","birth",'year','month','day','color'):
+            replySTR = "請問您是問哪一位成員呢？"
             
         else:  #給答案
             #Group
             if type(mscDICT[client.user.id]["group"])==list and mscDICT[client.user.id]["member"] =="":
                 if type(mscDICT[client.user.id]["request"]) == int:
                     replySTR = '共有 '+str(mscDICT[client.user.id]["request"])+' 團。'
-                    
-    
                 else:
                     answerSTR=""
                     for n in range(0,len(lokiResultDICT['Group'])):
@@ -227,6 +364,7 @@ async def on_message(message):
                 else: 
                     replySTR = mscDICT[client.user.id]["group"]+' 並不是Jstorm旗下的團體。'
             
+            
             #member
             elif type(mscDICT[client.user.id]["member"])==list:
                 if type(mscDICT[client.user.id]["request"])==list:
@@ -237,7 +375,7 @@ async def on_message(message):
                                 answerSTR+=lokiResultDICT['member'][n]+' 是 '+lokiResultDICT['request'][n]+'。\n'
                             replySTR = answerSTR
                             
-                            
+                        
                         else:  #問整團成員
                             answerSTR=""
                             for n in range(len(mscDICT[client.user.id]["member"])):
@@ -267,8 +405,91 @@ async def on_message(message):
                           
                 elif mscDICT[client.user.id]["request"] == 'no': #成員和團體沒對上
                     replySTR=f'不， {mscDICT[client.user.id]["member"]} 是 {mscDICT[client.user.id]["group"]} 的成員。'
+                
+                elif mscDICT[client.user.id]["request"] == 'weight': #問體重
+                    replySTR="怎麼可以問人家體重呢> <"
                     
-                 
+                #anniversary
+                elif mscDICT[client.user.id]["request"] in ('formationdate','debutdate','formationdate,years','formationdate.days','debutdate.years','debutdate.days'):
+                    if mscDICT[client.user.id]["request"]=='formationdate':#結成日
+                        date=findDate(mscDICT[client.user.id]["group"],'formationdate')
+                        if mscDICT[client.user.id]["group"] == 'TOKIO':
+                            replySTR="他們的結成日是 "+ date[:4]+"年 "+date[5:6]+"月。"
+                        else:
+                            replySTR="他們的結成日是 "+ date +"。"
+                    elif mscDICT[client.user.id]["request"]=='debutdate':#出道日
+                        date=findDate(mscDICT[client.user.id]["group"],'debutdate')
+                        replySTR="他們的出道日是 "+ date +"。"
+                        
+                    elif mscDICT[client.user.id]["request"]=='formationdate.years': #出道幾年
+                        years=countYears(mscDICT[client.user.id]["group"],'formationdate')
+                        replySTR="他們已經結成 "+ str(years) +"年了。"
+                            
+                    elif mscDICT[client.user.id]["request"]=='debutdate.years': #出道幾年
+                        years=countYears(mscDICT[client.user.id]["group"],'debutdate')
+                        replySTR="他們已經出道 "+ str(years) +"年了。"
+                        
+                    elif mscDICT[client.user.id]["request"]=='formationdate.days': #出道幾年
+                        days=countDays(mscDICT[client.user.id]["group"],'formationdate')
+                        replySTR="離結成日還有 "+ str(days) +"天。"
+                        
+                    elif mscDICT[client.user.id]["request"]=='debutdate.days': #出道幾年
+                        days=countDays(mscDICT[client.user.id]["group"],'debutdate')
+                        replySTR="離出道日還有 "+ str(days) +"天。"
+                        
+                #color
+                elif mscDICT[client.user.id]["request"] in ('color','color.list'):
+                    if mscDICT[client.user.id]["request"] == 'color': #問顏色
+                        color = findColor(mscDICT[client.user.id]["member"])
+                        replySTR=mscDICT[client.user.id]["member"]+' 的成員色是 '+color+'色。'
+                    elif mscDICT[client.user.id]["request"] == 'color.list': #問總共有什麼顏色
+                        colorLIST=findColorLIST(mscDICT[client.user.id]["group"])
+                        answerSTR=""
+                        if mscDICT[client.user.id]["group"] == 'TOKIO':
+                            for n in range(len(colorLIST)):
+                                answerSTR += tokio[n]+' 的成員色是 '+colorLIST[n]+'色。\n' 
+                        if mscDICT[client.user.id]["group"] == '嵐':
+                            for n in range(len(colorLIST)):
+                                answerSTR += arashi[n]+' 的成員色是 '+colorLIST[n]+'色。\n'                            
+                        if mscDICT[client.user.id]["group"] == 'KAT-TUN':
+                            for n in range(len(colorLIST)):
+                                answerSTR += kattun[n]+' 的成員色是 '+colorLIST[n]+'色。\n'                          
+                        if mscDICT[client.user.id]["group"] == 'Hey! Say! JUMP':
+                            for n in range(len(colorLIST)):
+                                answerSTR += jump[n]+' 的成員色是 '+colorLIST[n]+'色。\n'
+                        
+                        replySTR=answerSTR
+                
+                elif mscDICT[client.user.id]["request"] in ('紅', '橙', '黃', '綠', '藍', '紫', '天藍', '粉', '粉紅', '黃綠', '淺綠', '草綠', '淡綠'):  
+                    #有沒有人是這個顏色
+                    if mscDICT[client.user.id]["group"] == 'TOKIO':
+                        tokio_color=findColorLIST('TOKIO')
+                        if mscDICT[client.user.id]["request"] in tokio_color:
+                            index=tokio.index(mscDICT[client.user.id]["request"])
+                            member=tokio[index]
+                            replySTR=member+' 的成員色是 '+mscDICT[client.user.id]["request"]+'色。\n'                    
+                    elif mscDICT[client.user.id]["group"] == '嵐':
+                        arashi_color=findColorLIST('嵐')
+                        if mscDICT[client.user.id]["request"] in arashi_color:
+                            index=arashi.index(mscDICT[client.user.id]["request"])
+                            member=arashi[index]
+                            replySTR=member+' 的成員色是 '+mscDICT[client.user.id]["request"]+'色。\n'                         
+                    elif mscDICT[client.user.id]["group"] == 'KAT-TUN':
+                        kattun_color=findColorLIST('KAT-TUN')
+                        if mscDICT[client.user.id]["request"] in kattun_color:
+                            index=kattun.index(mscDICT[client.user.id]["request"])
+                            member=kattun[index]
+                            replySTR=member+' 的成員色是 '+mscDICT[client.user.id]["request"]+'色。\n'                       
+                    elif mscDICT[client.user.id]["group"] == 'Hey! Say! JUMP':
+                        jump_color=findColorLIST('Hey! Say! JUMP')
+                        if mscDICT[client.user.id]["request"] in jump_color:
+                            index=jump.index(mscDICT[client.user.id]["request"])
+                            member=jump[index]
+                            replySTR=member+' 的成員色是 '+mscDICT[client.user.id]["request"]+'色。\n'
+                
+                elif mscDICT[client.user.id]["request"] == 'no.color': #沒人是這個顏色
+                    replySTR='沒有人的代表色是這個顏色。'
+                
                 #name
                 elif mscDICT[client.user.id]["request"] == 'yes.group': #有這個人
                     replySTR=f'有的， {mscDICT[client.user.id]["member"]} 是 {mscDICT[client.user.id]["group"]} 的成員。'
@@ -413,7 +634,7 @@ async def on_message(message):
                         answerSTR=""
                         for n in indexLIST:
                             answerSTR += ProfileDICT[mscDICT[client.user.id]["group"]][n]['JName']+"，"+str(ProfileDICT[mscDICT[client.user.id]["group"]][n]['height'])+"cm\n"
-                        replySTR = "身高從矮到高是：\n"+answerSTR
+                            replySTR = "身高從矮到高是：\n"+answerSTR
                         
                         
                     elif mscDICT[client.user.id]["request"] =="height.sort.HtoL":  #高到矮
@@ -433,7 +654,7 @@ async def on_message(message):
                         answerSTR=""
                         for n in indexLIST:
                             answerSTR += ProfileDICT[mscDICT[client.user.id]["group"]][n]['JName']+"，"+str(ProfileDICT[mscDICT[client.user.id]["group"]][n]['height'])+"cm\n"
-                        replySTR = "身高從高到矮是：\n"+answerSTR
+                            replySTR = "身高從高到矮是：\n"+answerSTR
                         
                        
                 elif mscDICT[client.user.id]["request"].encode('UTF-8').isalnum() == True: #request是身高
